6db49c01593d9d6d3ddfdfeca90aff988cdc177f
moartaee <moartae@gmail.com>
2025-11-13 14:43:50 -0500

    no error lmao

--- bubble/src/main/java/com/bubblewrap/bubble/DirectDecrypter.java
+++ bubble/src/main/java/com/bubblewrap/bubble/DirectDecrypter.java
@@ -3,6 +3,7 @@ package com.bubblewrap.bubble;
 import java.math.BigInteger;
 
 public class DirectDecrypter {
-    BigInteger key = DirectEncrypter.getKey();
-    BigInteger encrypter =
+    public void decrypt(BigInteger key, String fileName) {
+        //read from file and get the encrypted password, then divide the encrypted password using the key to get the password before encryption
+    }
 }
