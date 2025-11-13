3a1c3b97b3540086708e8f51227b3d9b4eed7ba7
Thomas Alexandrescu <thomasalexandrescu@gmail.com>
2025-11-13 15:42:34 -0500

    Encrypt and decrypt methods call work

--- bubble/src/main/java/com/bubblewrap/bubble/DirectEncrypter.java
+++ bubble/src/main/java/com/bubblewrap/bubble/DirectEncrypter.java
@@ -80,7 +80,7 @@
         }
 
         public static void main(String[] args) {
-            DirectEncrypter encrypter = new DirectEncrypter("test", "datafile.txt", "Instagram", "password123", new DirectDecrypter());
+            DirectEncrypter encrypter = new DirectEncrypter("test", "instagram.txt", "Instagram", "password123", new DirectDecrypter());
             encrypter.decrypt();
         }
     }
