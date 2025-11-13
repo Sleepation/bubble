b6d9c64a3c4e91824380912fda10bb9a81e81cf1
moartaee <moartae@gmail.com>
2025-11-13 13:45:47 -0500

    added emojicons

--- bubble/src/main/java/com/bubblewrap/bubble/BubbleApplication.java
+++ bubble/src/main/java/com/bubblewrap/bubble/BubbleApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BubbleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BubbleApplication.class, args);
+
+        SpringApplication.run(BubbleApplication.class, args);
+
 	}
 
 }
